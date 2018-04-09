f4b90a4c4b074d5b0a8a73ebe4538216c52ba2b1
EnricoSchw <enrico.schw@gmx.de>
2018-04-09 20:58:50 +0200

    Add management modul with calendar

---
+++ src/main/webapp/app/management/calendar/calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-calendar',
+  templateUrl: './calendar.component.html',
+  styles: []
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
