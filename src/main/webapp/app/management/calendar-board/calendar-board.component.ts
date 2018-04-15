f716c9a29008052d785c75af1da210b90ab6a875
EnricoSchw <enrico.schw@gmx.de>
2018-04-15 20:17:31 +0200

    calendar dashboard

---
+++ src/main/webapp/app/management/calendar-board/calendar-board.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-calendar-board',
+    templateUrl: './calendar-board.component.html',
+    styleUrls: ['./calendar-board.scss']
+})
+export class CalendarBoardComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
--- src/main/webapp/app/management/management.module.ts
+++ src/main/webapp/app/management/management.module.ts
@@ -2,14 +2,15 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarModule } from 'angular-calendar';
+import { CalendarBoardComponent } from './calendar-board/calendar-board.component';
 
 @NgModule({
     imports: [
         CommonModule,
         CalendarModule.forRoot()
     ],
-    declarations: [CalendarComponent],
-    exports: [CalendarComponent],
+    declarations: [CalendarComponent, CalendarBoardComponent],
+    exports: [CalendarComponent, CalendarBoardComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ManagementModule {
