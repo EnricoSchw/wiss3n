0a87b8586497913b5df88c0ccdba04ceba3e0e41
EnricoSchw <enrico.schw@gmx.de>
2018-04-30 22:28:09 +0200

    Custemize Calendar Week view

---
+++ src/main/webapp/app/management/calendar/week/custom-calendar-week-view/custom-calendar-week-view.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { CalendarWeekViewComponent } from 'angular-calendar';
+
+@Component({
+  selector: 'jhi-custom-calendar-week-view',
+  templateUrl: './custom-calendar-week-view.component.html',
+  styles: []
+})
+export class CustomCalendarWeekViewComponent extends CalendarWeekViewComponent{
+}
--- src/main/webapp/app/management/management.module.ts
+++ src/main/webapp/app/management/management.module.ts
@@ -4,13 +4,14 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarModule } from 'angular-calendar';
 import { CalendarBoardComponent } from './calendar-board/calendar-board.component';
 import { CalendarService } from './providers/calendar.service';
+import { CustomCalendarWeekViewComponent } from './calendar/week/custom-calendar-week-view/custom-calendar-week-view.component';
 
 @NgModule({
     imports: [
         CommonModule,
         CalendarModule.forRoot()
     ],
-    declarations: [CalendarComponent, CalendarBoardComponent],
+    declarations: [CalendarComponent, CalendarBoardComponent, CustomCalendarWeekViewComponent],
     providers: [CalendarService],
     exports: [CalendarBoardComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
