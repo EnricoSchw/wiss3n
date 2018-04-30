284840c4b62d3234bea2cb8fc9ae720cdb9c3082
EnricoSchw <enrico.schw@gmx.de>
2018-04-30 23:26:56 +0200

    Change hore class to const

--- src/main/webapp/app/management/models/subject-hour.ts
+++ src/main/webapp/app/management/models/subject-hour.ts
@@ -3,38 +3,38 @@ export interface SubjectHour {
     end: Date,
 }
 
-export class FirstHour implements SubjectHour{
-    public start = new Date('2018-04-24T08:15:00');
-    public end = new Date('2018-04-24T09:00:00');
-}
+export const firstHour = <SubjectHour>{
+    start: new Date('2018-04-24T08:15:00'),
+    end:  new Date('2018-04-24T09:00:00')
+};
 
-export class SecondHour implements SubjectHour{
-    public start = new Date('2018-04-24T09:05:00');
-    public end = new Date('2018-04-24T09:50:00');
-}
+export const secondHour = <SubjectHour> {
+     start : new Date('2018-04-24T09:05:00'),
+     end : new Date('2018-04-24T09:50:00')
+};
 
-export class ThirdHour implements SubjectHour{
-    public start = new Date('2018-04-24T10:00:00');
-    public end = new Date('2018-04-24T10:50:00');
-}
+export const thirdHour = <SubjectHour> {
+    start: new Date('2018-04-24T10:00:00'),
+    end: new Date('2018-04-24T10:50:00')
+};
 
-export class FourthHour implements SubjectHour{
-    public start = new Date('2018-04-24T11:00:00');
-    public end = new Date('2018-04-24T11:45:00');
-}
+export const fourthHour = <SubjectHour> {
+    start: new Date('2018-04-24T11:00:00'),
+    end: new Date('2018-04-24T11:45:00')
+};
 
 
-export class FifthHour implements SubjectHour{
-    public start = new Date('2018-04-24T12:05:00');
-    public end = new Date('2018-04-24T12:50:00');
-}
+export const fifthHour = <SubjectHour> {
+    start: new Date('2018-04-24T12:05:00'),
+    end: new Date('2018-04-24T12:50:00')
+};
 
-export class SixthHour implements SubjectHour{
-    public start = new Date('2018-04-24T12:55:00');
-    public end = new Date('2018-04-24T13:10:00');
-}
+export const sixthHour = <SubjectHour> {
+    start: new Date('2018-04-24T12:55:00'),
+    end: new Date('2018-04-24T13:10:00')
+};
 
-export class SeventhHour implements SubjectHour{
-    public start = new Date('2018-04-24T13:45:00');
-    public end = new Date('2018-04-24T14:35:00');
-}
+export const seventhHour = <SubjectHour> {
+    start: new Date('2018-04-24T13:45:00'),
+    end: new Date('2018-04-24T14:35:00')
+};
