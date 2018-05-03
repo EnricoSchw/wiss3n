43a1cd4186ac3e5736b97937a2f2d0e747aba989
EnricoSchw <enrico.schw@gmx.de>
2018-05-03 07:41:53 +0200

    New subject list interface

--- src/main/webapp/app/management/fixtures/subjects.ts
+++ src/main/webapp/app/management/fixtures/subjects.ts
@@ -3,7 +3,16 @@ import { endOfWeek, startOfWeek } from 'date-fns';
 import { RRule } from 'rrule';
 import { fifthHour, firstHour, fourthHour, secondHour, sixthHour, thirdHour } from '../models/subject-hour';
 import { RecurringEvent } from '../models/events';
+import { WeekDay } from '@angular/common';
 
+
+interface SubjectHourData {
+    id: number;
+    title: string,
+    prefix: string,
+    hour: WeekDay,
+    color: EventColor
+}
 
 const subjectColor = <EventColor>{
     primary: '#284451',
@@ -14,15 +23,16 @@ const setTime = (time: string): Date => {
     return new Date('2018-04-01T' + time);
 };
 
+const createSubjectEventList = (subjectHourList: SubjectHourData[]) => {
 
-const subjectHourList = [
+};
+
+const subjectHourList = <SubjectHourData[]>[
     {id: 1, title: 'MusiK', prefix: 'MU', hour: 1, color: subjectColor},
     {id: 1, title: 'MusiK', prefix: 'MU', hour: 1, color: subjectColor},
-    {id: 2, title: 'Naturwissenschaft', prefix: 'Nawi', day: RRule.MO, hour: 2, color: subjectColor}
+    {id: 2, title: 'Naturwissenschaft', prefix: 'Nawi', day: RRule.MO, hour: 2, color: subjectColor},
     {id: 1, title: 'MusiK', prefix: 'MU', hour: 1, color: subjectColor}
 ];
-
-
 
 
 const naviMonday = <RecurringEvent>{
