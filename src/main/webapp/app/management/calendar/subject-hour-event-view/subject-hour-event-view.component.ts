b75dbdb148e0fb971cedb8f8f2f47a3d602ef34b
EnricoSchw <enrico.schw@gmx.de>
2018-06-03 12:21:04 +0200

    Add loop logic in date view

--- src/main/webapp/app/management/calendar/subject-hour-event-view/subject-hour-event-view.component.ts
+++ src/main/webapp/app/management/calendar/subject-hour-event-view/subject-hour-event-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TaskEventMeta } from '../../models/events';
+import { CalendarEvent } from 'calendar-utils';
 
 @Component({
     selector: 'jhi-subject-hour-event-view',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./subject-hour-event-view.component.scss']
 })
 export class SubjectHourEventViewComponent implements OnInit {
+
+    @Input() event: CalendarEvent<TaskEventMeta>;
 
     constructor() {
     }
