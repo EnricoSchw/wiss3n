105f8b2b80c674b5e8b47526ee8a4ca1f277c7a7
EnricoSchw <enrico.schw@gmx.de>
2018-07-08 13:11:48 +0200

    Add old hom modeul

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { Wiss3NSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { InfoComponent } from 'app/home/info/info.component';
 
 @NgModule({
     imports: [Wiss3NSharedModule, RouterModule.forChild([HOME_ROUTE])],
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, InfoComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Wiss3NHomeModule {}
