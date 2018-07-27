a915eae9cca50531d2029fbd1dfaacd1bd36a418
EnricoSchw <enrico.schw@gmx.de>
2018-07-27 12:38:05 +0200

    Add new git hub symbol

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html',
--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -40,6 +40,8 @@ import {
     faHome
 } from '@fortawesome/free-solid-svg-icons';
 
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 // Adds the SVG icon to the library so you can use it in your page
 library.add(faUser);
 library.add(faSort);
@@ -73,5 +75,6 @@ library.add(faCalendarAlt);
 library.add(faSearch);
 library.add(faTrashAlt);
 library.add(faAsterisk);
+library.add(faGithub);
 
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
