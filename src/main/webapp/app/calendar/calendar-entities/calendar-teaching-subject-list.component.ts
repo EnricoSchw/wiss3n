900e88ad6b3d6793fcdd687ba610174994464b7d
EnricoSchw <enrico.schw@gmx.de>
2018-08-08 22:29:46 +0200

    Add scholl class id condition

--- src/main/webapp/app/calendar/calendar-entities/calendar-teaching-subject-list.component.ts
+++ src/main/webapp/app/calendar/calendar-entities/calendar-teaching-subject-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarSubjectEventStoreService } from 'app/store/calendar-subject-event/calendar-subject-event-store.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'calendar-teaching-subject-list',
-  templateUrl: './calendar-teaching-subject-list.component.html',
-  styleUrls: ['./calendar-teaching-subject-list.component.scss']
+    selector: 'calendar-teaching-subject-list',
+    templateUrl: './calendar-teaching-subject-list.component.html',
+    styleUrls: ['./calendar-teaching-subject-list.component.scss']
 })
 export class CalendarTeachingSubjectListComponent implements OnInit {
+    private schoolClassId$: Observable<number>;
 
-  constructor() { }
+    constructor(private storeService: CalendarSubjectEventStoreService) {
 
-  ngOnInit() {
-  }
+    }
+
+    ngOnInit() {
+        this.schoolClassId$ = this.storeService.getActiveSchoolClassId();
+    }
 
 }
