9f2bf4a7ef65b72637cb3d228b7befd5e894263e
EnricoSchw <enrico.schw@gmx.de>
2018-12-31 10:57:43 +0100

    Move view templates

--- src/main/webapp/app/calendar/components/calendar-view-day/calendar-view-day.component.ts
+++ src/main/webapp/app/calendar/components/calendar-view-day/calendar-view-day.component.ts
@@ -1,9 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { CalendarEvent, ViewPeriod } from 'calendar-utils';
+
 
 @Component({
   selector: 'jhi-calendar-view-day',
   templateUrl: './calendar-view-day.component.html',
-  styleUrls: ['./calendar-view-day.component.scss']
+  styleUrls: ['./calendar-view-day.component.scss', './../../calendar.component.scss', './../../scss/media-queries.scss']
 })
 export class CalendarViewDayComponent implements OnInit {
 
@@ -20,4 +22,16 @@ export class CalendarViewDayComponent implements OnInit {
         alert('LessonClicked');
     }
 
+
+    public beforeDayViewRender({period}: { period: ViewPeriod }): void {
+        // const events: SubjectEvent[] = <SubjectEvent[]> period.events;
+        // if (events.length > 0) {
+        //     if ((this.activeEvent === null || this.activeEvent === undefined)) {
+        //         this.setActiveEvent(events[0]);
+        //     }
+        //     if (!isSameDay(this.activeEvent.start, this.viewDate)) {
+        //         this.setActiveEvent(events[0]);
+        //     }
+        // }
+    }
 }
